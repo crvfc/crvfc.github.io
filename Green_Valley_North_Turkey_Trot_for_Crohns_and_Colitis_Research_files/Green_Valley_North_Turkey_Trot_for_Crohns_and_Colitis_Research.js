a08c8ed98d4e3abd6f1b22ac8896babd1590396b
Matthew Glace <mglace@Matthews-MacBook-Pro.local>
2016-10-19 20:38:48 -0400

    files added

---
+++ Green_Valley_North_Turkey_Trot_for_Crohns_and_Colitis_Research_files/Green_Valley_North_Turkey_Trot_for_Crohns_and_Colitis_Research.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20141028
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:38,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:1.000000}),shadow_1:new IWShadow({blurRadius:38,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:1.000000}),stroke_0:new IWEmptyStroke(),shadow_2:new IWShadow({blurRadius:33,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:1.000000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Green_Valley_North_Turkey_Trot_for_Crohns_and_Colitis_Research_files/Green_Valley_North_Turkey_Trot_for_Crohns_and_Colitis_ResearchMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');detectBrowser();adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');adjustLineHeightIfTooBig('id14');adjustFontSizeIfTooBig('id14');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
